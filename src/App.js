aa3bddae260b2ee331ceb774e13d177ca2858a7e
Nathan <novadrake76@gmail.com>
2022-07-24 07:51:34 -0300

    feat: case render progress

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Sidebar from './components/sidebar';
-import Crash from './components/crash';
-import Roulette from './components/roulette';
-import React, { useState} from 'react';
+import "./App.css"
+import Navbar from "./components/navbar"
+import Sidebar from "./components/sidebar"
+import Crash from "./components/crash"
+import Roulette from "./components/roulette"
+import React, { useState } from "react"
 
-function App() {  
-
-  let gameAux = 0;
-  if (window.location.pathname === '/roulette') {
-     gameAux = 0;
-  } else if (window.location.pathname === '/crash') {
-     gameAux = 1;
+function App() {
+  let gameAux = 0
+  if (window.location.pathname === "/roulette") {
+    gameAux = 0
+  } else if (window.location.pathname === "/crash") {
+    gameAux = 1
   }
-  
-  const [game, setGame] = useState(gameAux);
+
+  const [game, setGame] = useState(gameAux)
   const pullGame = (game) => {
-    setGame(game);
+    setGame(game)
   }
 
-  const renderGame = game => (game ?  <Crash/> : <Roulette/>);
-  const Content = ({game}) => <div>{renderGame(game)}</div>
+  const renderGame = (game) => (game ? <Crash /> : <Roulette />)
+  const Content = ({ game }) => <div>{renderGame(game)}</div>
 
-    return (
-      <div className="flex flex-col App">
-        <Navbar />
-        <div className="flex w-full h-screen divide-x divide-gray-500 bg-slate-600">
-          <Sidebar func={pullGame}/>
-          <div className="flex">
-            <Content game={game} />
-          </div>
-          
+  return (
+    <div className="flex flex-col App">
+      <Navbar />
+      <div className="flex w-full h-screen divide-x divide-gray-500 bg-slate-600">
+        <Sidebar func={pullGame} />
+        <div className="">
+          <Content game={game} />
         </div>
       </div>
-    );
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/roulette.js
+++ src/components/roulette.js
@@ -1,83 +1,173 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
 const prizes = [
-  {"case": [{
-    name: '1st',
-    image: 'https://i.ibb.co/6Z6Xm9d/good-1.png',
-    probability: 0.1,
+  {
+    name: "Dogs Case",
+    case: [
+      {
+        name: "1st",
+        image:
+          "https://images.dog.ceo/breeds/mountain-bernese/n02107683_4494.jpg",
+        probability: 0.1,
+      },
+      {
+        name: "2st",
+        image: "https://images.dog.ceo/breeds/terrier-wheaten/n02098105_97.jpg",
+        probability: 0.1,
+      },
+      {
+        name: "3st",
+        image: "https://images.dog.ceo/breeds/african/n02116738_1739.jpg",
+        probability: 0.2,
+      },
+      {
+        name: "4st",
+        image: "https://images.dog.ceo/breeds/weimaraner/n02092339_6209.jpg",
+        probability: 0.2,
+      },
+      {
+        name: "5st",
+        image: "https://images.dog.ceo/breeds/hound-ibizan/n02091244_2838.jpg",
+        probability: 0.2,
+      },
+      {
+        name: "6st",
+        image: "https://images.dog.ceo/breeds/pug/n02110958_10186.jpg",
+        probability: 0.2,
+      },
+    ],
   },
   {
-    name: '2st',
-    image: 'https://i.ibb.co/6Z6Xm9d/good-1.png',
-    probability: 0.1,
+    name: "Scarlet Case",
+    case: [
+      {
+        name: "1st",
+        image:
+          "https://en.touhouwiki.net/images/thumb/c/c8/Th18Reimu.png/278px-Th18Reimu.png",
+        probability: 0.1,
+      },
+      {
+        name: "2st",
+        image:
+          "https://en.touhouwiki.net/images/thumb/c/cd/Th18Marisa.png/278px-Th18Marisa.png",
+        probability: 0.1,
+      },
+      {
+        name: "3st",
+        image:
+          "https://en.touhouwiki.net/images/thumb/d/d5/Th14Cirno.png/235px-Th14Cirno.png",
+        probability: 0.2,
+      },
+      {
+        name: "4st",
+        image:
+          "https://en.touhouwiki.net/images/thumb/9/99/Th105Patchouli.png/275px-Th105Patchouli.png",
+        probability: 0.2,
+      },
+      {
+        name: "5st",
+        image:
+          "https://en.touhouwiki.net/images/thumb/6/62/Th18Sakuya.png/276px-Th18Sakuya.png",
+        probability: 0.2,
+      },
+      {
+        name: "6st",
+        image:
+          "https://en.touhouwiki.net/images/thumb/e/e7/Th105Remilia.png/275px-Th105Remilia.png",
+        probability: 0.2,
+      },
+    ],
   },
   {
-    name: '3st',
-    image: 'https://i.ibb.co/T1M05LR/good-2.png',
-    probability: 0.2,
+    name: "Case 3",
+    case: [
+      {
+        name: "1st",
+        image: "https://i.ibb.co/6Z6Xm9d/good-1.png",
+        probability: 0.1,
+      },
+      {
+        name: "2st",
+        image: "https://i.ibb.co/6Z6Xm9d/good-1.png",
+        probability: 0.1,
+      },
+      {
+        name: "3st",
+        image: "https://i.ibb.co/T1M05LR/good-2.png",
+        probability: 0.2,
+      },
+      {
+        name: "4st",
+        image: "https://i.ibb.co/Qbm8cNL/good-3.png",
+        probability: 0.2,
+      },
+      {
+        name: "5st",
+        image: "https://i.ibb.co/5Tpfs6W/good-4.png",
+        probability: 0.2,
+      },
+      {
+        name: "6st",
+        image: "https://i.ibb.co/64k8D1c/good-5.png",
+        probability: 0.2,
+      },
+    ],
   },
-  {
-    name: '4st',
-    image: 'https://i.ibb.co/Qbm8cNL/good-3.png',
-    probability: 0.2,
-  },
-  {
-    name: '5st',
-    image: 'https://i.ibb.co/5Tpfs6W/good-4.png',
-    probability: 0.2,
-  },
-  {
-    name: '6st',
-    image: 'https://i.ibb.co/64k8D1c/good-5.png',
-    probability: 0.2,
-  },]}
-];
-
+]
 
 const Roulette = () => {
-  const [container, setContainer] = useState(0);
-  const [prize, setPrize] = useState(null);
-  let prizeRenderAux;
+  const [container, setContainer] = useState(0)
+  const [prize, setPrize] = useState(null)
+  const [prizeDefined, setPrizeDefined] = useState(false)
+  let prizeRenderAux
+  let caseRender
 
-  if (prize) {
-    prizeRenderAux = <div>
-      <h1>You won {prize.name}</h1>
-      <img src={prize.image} alt="prize" />
-    </div>
+  caseRender = prizes.map((prize, index) => {
+    return (
+      <button key={index} onClick={() => setContainer(index)}>
+        {prize.name}
+      </button>
+    )
+  })
+
+  if (prizeDefined === true) {
+    prizeRenderAux = (
+      <div className="flex flex-col justify-center align-center">
+        <h1>You won {prize.name}</h1>
+        <img className="flex self-center w-2/4" src={prize.image} alt="prize" />
+      </div>
+    )
   }
 
   const getRandomPrize = () => {
-    document.getElementById('spin').disabled = true;
+    setPrizeDefined(false)
+    document.getElementById("spin").disabled = true
 
-    setPrize(prizes[container].case[Math.floor(Math.random() * prizes[container].case.length)]);
+    setPrize(
+      prizes[container].case[
+        Math.floor(Math.random() * prizes[container].case.length)
+      ]
+    )
+
+    setPrizeDefined(true)
 
     setTimeout(() => {
-      document.getElementById('spin').disabled = false;
-
-    }, 1000);
-    }
-
-  useEffect(() => {
-    getRandomPrize();
-  }, [container]);
-  
-
+      document.getElementById("spin").disabled = false
+    }, 300)
+  }
 
   return (
     <>
-      <div className="flex flex-col justify-center gap-4">
-        <div className='flex'>Roulette</div>
-        <div className="flex">
-        <button onClick={getRandomPrize}>Case 1</button>
-
-        </div>
-          <button id='spin' onClick={getRandomPrize} >Spin</button>
-            {prizeRenderAux}
+      <div className="flex flex-col justify-center w-[80vw] gap-4">
+        <div className="flex justify-center">Roulette</div>
+        <div className="flex gap-5 ">{caseRender}</div>
+        <button id="spin" onClick={getRandomPrize}>
+          Spin
+        </button>
+        {prizeRenderAux}
       </div>
     </>
-  );
-};
+  )
+}
 
-
-
-export default Roulette;
+export default Roulette
