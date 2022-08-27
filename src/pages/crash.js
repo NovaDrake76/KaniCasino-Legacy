4080321040eceaa386bc4c37f72fb11f1e266155
Nathan <novadrake76@gmail.com>
2022-08-27 01:00:02 -0300

    feat: jashin

--- src/pages/crash.js
+++ src/pages/crash.js
@@ -1,26 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Helmet } from "react-helmet"
 
 const Crash = () => {
-  // const [count, setCount] = useState(1)
-  // const [crasher, setCrasher] = useState()
+  const [count, setCount] = useState(1)
+  const [crasher, setCrasher] = useState()
 
-  //generate a random number
-  // const randomNumber = () => {
-  //   return Math.floor(Math.random() * 100)
-  // }
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 100)
+  }
 
-  // useEffect(() => {
-  //   setCrasher(randomNumber())
+  useEffect(() => {
+    setCrasher(randomNumber())
 
-  //   while (count !== crasher) {
-  //     setTimeout(() => {
-  //       setCount(count + 0.01)
-  //     }, 1000)
-  //   }
+    while (count !== crasher) {
+      setTimeout(() => {
+        setCount(count + 0.01)
+      }, 1000)
+    }
 
-  //   setCount(parseFloat(count.toFixed(2)))
-  // }, [count, crasher])
+    setCount(parseFloat(count.toFixed(2)))
+  }, [count, crasher])
 
   return (
     <>
@@ -28,12 +27,13 @@ const Crash = () => {
         <title>Crash | KaniCasino</title>
       </Helmet>
       <div className="flex flex-col items-center">
-        <span>In development...</span>
+        {/* <span>In development...</span>
         <img
           src="https://i.imgur.com/kj3fL6c.png"
           alt="monkey"
           className="max-w-10"
-        />
+        /> */}
+        <span className="text-2xl text-gray-600">{count}</span>
       </div>
     </>
   )
